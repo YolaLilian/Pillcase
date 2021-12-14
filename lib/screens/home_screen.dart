83c94e8285a6720056724cee55bd76d3a90c6dc8
Blackreaper5 <amaterasu666111@hotmail.com>
2021-12-14 18:23:30 +0100

    Add missing home screen and main code

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pill_case/view_models/pill_list_view_model.dart';
+import 'package:pill_case/view_models/pill_view_model.dart';
 import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -15,18 +16,56 @@ class _HomeScreenState extends State<HomeScreen> {
         .updatePillViewModels();
   }
 
-  // Card buildCard(PillListViewModel pill_list_view_model) {
-  //   Text.
-  // }
+  Widget getTextWidgets(List<String> strings)
+  {
+    return new Row(children: strings.map((item) => new Text(item)).toList());
+  }
+
+  Card _buildCard(PillViewModel pillViewModel){
+      // TODO
+      // Komt alle visuele zooi van de card in
+      var name =  pillViewModel.name;
+      var time = "${pillViewModel.dateTime.hour}:${pillViewModel.dateTime.minute}";
+      var status = pillViewModel.status;
+
+      return Card( 
+        elevation: 4.0,
+        child: Column(
+          children: [
+            title: Text(name),
+            subtitle: Text(time),
+
+            switch (status) {
+              case status.completed:
+                return (
+                  color: Colors.green;
+                );
+              case status.upcoming: 
+                return (
+                  color: Colors.orange;
+                );
+              case status.failed:
+                return ( 
+                  color: Colors.red;
+                );
+            }
+          ],
+        )
+      );
+  }
+
+  Widget _getPills() {
+    //TODO
+    // Komt de loop van pillcards in
+    // cards = pill_list_view_model.pillViewModels.map((pill_view_model) => buildCard(pill_view_model))
+  }
+
 
   Widget _buildList(PillListViewModel pill_list_view_model) {
     return ListView(
       padding: const EdgeInsets.all(8),
       children: <Widget>[
-        // for (var pillViewModel in pill_list_view_model)
-        // ListTile(
-        // title: Text(pillViewModel.name)))
-        ListTile(title: Text(map(pill_list_view_model.pillViewModels.name))),
+        Card(title: Text(pill_list_view_model.pillViewModels.map((pill_view_model) => null))),
       ],
     );
   }
@@ -38,7 +77,7 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: const Text('Pillen overzicht van vandaag'),
       ),
-      body: _buildList(pill_list_view_model),
+      body: _getPills(),
     );
   }
 }
