aa15c32bf9a50a6d4ee90ce0059a92d08d1b2419
Blackreaper5 <amaterasu666111@hotmail.com>
2021-12-19 14:24:06 +0100

    Add FirebaseService function templates

---
+++ lib/services/firebase_service.dart
@@ -0,0 +1,16 @@
+class FirebaseService {
+  // NOTE: New functions may need to be added or current ones need to be updated
+
+  void createAccount(email, password) => "";// TODO
+
+  void logIn(email, password) => "";// TODO
+
+  void resetPassword(email) => ""; // TODO
+
+  void updatePassword(oldPassword, newPassword) => "";// TODO
+
+  void updateEmail(newEmail) => "";// TODO
+
+  String getEmail() => "";// TODO
+
+}
